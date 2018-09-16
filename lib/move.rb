2dc1a09be013efb0709079b9d3fbbc29b660cdae
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-16 16:52:50 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,7 +14,7 @@ end
 def move(array, index, value = "X")
   board = [" ", " ", " "]
   def update_array_at_with(array, index, value)
-    array[index] = value
+    array[input_to_index(index)] = value
   end
   
   update_array_at_with(board, 0, "X")
