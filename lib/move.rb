3ca98a616a3b801c2a299e615878c051e23d514c
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-16 16:42:10 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -20,6 +20,4 @@ def move(array, index, value = "X")
   end
   
   update_array_at_with(board, 0, "X")
-# The element at index 0 of array 'board' is set to the value "X"
-  board #=> ["X", " ", " "]
 end
